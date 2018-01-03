0fdaac002c7a4c2c902c1b588a69b6d86f7393b0
S.Pragyan Panigrahi <s.pragyan2008@hotmail.com>
2018-01-02 23:37:09 -0800

    Print7 Project Added

---
+++ Fibonacci/Fibonnaci.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Fibonnaci
+    {
+        static void Main(string[] args)
+        {
+            //int n = Convert.ToInt32(args[0]);
+            int n = 4;
+
+            FibonnaciNumber(n);
+
+            Console.WriteLine("");
+        }
+
+        private static int FibonnaciNumber(int n)
+        {
+            int result = 0;
+            if (n <= 0)
+            {
+                result = 0;
+            }
+            else if (n == 1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = FibonnaciNumber(n - 1) + FibonnaciNumber(n - 2);
+            }
+
+            Console.Write("{0} ", result);
+            return result;
+        }
+    }
+}
---
+++ Print7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(args[0]);
+
+            for (int i=1; i<=n; i++)
+            {
+                if (Is7(i))
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+        }
+
+        private static bool Is7(int i)
+        {
+            if ((i % 7) == 0)
+            {
+                return true;
+            }
+
+            while (i > 0)
+            {
+                int digit = i % 10;
+                if (digit == 7)
+                {
+                    return true;
+                }
+
+                i = i / 10;
+            }
+
+            return false;
+        }
+    }
+}
